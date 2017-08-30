af6770bc8f6d6c8c834a5de2a729d685c7bdbe0d
Nathan <nfriend@rsglab.com>
2017-08-30 07:36:54 -0400

    added src class

--- src/com/nfriend/Test.groovy
+++ src/com/nfriend/Test.groovy
@@ -2,8 +2,14 @@ package com.nfriend
 
 class Test {
 
-    Test(){
+    private final someString
 
+    Test(String someString){
+        this.someString = someString
+    }
+
+    String getSomeString() {
+        return this.someString
     }
 
 }
--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,13 @@
 #!/usr/bin/env groovy
 
+import com.nfriend.Test;
+
+testClass = new Test("I Got This From The Test Class!")
 
 def test() {
     echo "Testing"
+}
+
+def echoTest() {
+    echo testClass.echoSomeString()
 }
